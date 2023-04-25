c5afac7c1a411a61b742c43a6d15e22c428fe82e
Ayush <ydvayush06@gmail.com>
2023-04-26 00:59:58 +0530

    postcss and autoprexier issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-export default {
+export default { // 
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+//PostCSS and Autoprefixer are tools that help you write CSS code that is compatible with different browsers. website was not showing css before installing postcss and autoprefixer
