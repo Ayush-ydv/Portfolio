ad7d1fd2718a758d8b4d43c9100355f283ddb148
Ayush <ydvayush06@gmail.com>
2023-05-04 00:51:32 +0530

    change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, //service id
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID, //template id
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
