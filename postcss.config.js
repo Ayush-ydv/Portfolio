c092acf0035e685e42259ab82d09a05260290065
Ayush <ydvayush06@gmail.com>
2023-05-04 16:40:45 +0530

    postcss and autoprexier issue

--- postcss.config.js
+++ postcss.config.js
@@ -5,4 +5,4 @@ export default { //
   },
 }
 
-//PostCSS and Autoprefixer are tools that help you write CSS code that is compatible with different browsers. website was not showing css before installing postcss and autoprefixer
+//PostCSS and Autoprefixer are tools that help you write CSS code that is compatible with different browsers. website was not showing css before installing postcss and autoprefixer.
